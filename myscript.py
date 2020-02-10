97f90b5d120ad88dc3feeff72b88f82872e2d429
Jayati Thakor <thakorjayati7@gmail.com>
2020-02-10 11:26:56 -0800

    fixed the function and it uses try/except block for matrixes of diff sizes

--- myscript.py
+++ myscript.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 def function(matrix1,matrix2):
-    return matrix1 * matrix2
+    try:
+        return matrix1 * matrix2
+    except ValueError:
+        return None
 
 my_matrix =np.identity(4)
 second_matrix=np.random.random(size=(4,4))
-print(function(my_matrix,second_matrix)
+print(function(my_matrix,second_matrix))
